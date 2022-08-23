c192aab376f2808f917173b2152140bc43ae91ac
Berat Agaca <berat0925@outlook.com>
2022-08-23 22:36:53 +0300

    first commit

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api' : {
+          target: 'http://localhost:8080',
+          ws: true,
+          changeOrigin: true
+      }
+    }
+  }
+
+
+})
